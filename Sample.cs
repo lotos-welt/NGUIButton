704fea3e9e207ff73ec2a26e778354185120079e
Lotos <lotos@lotoslabo.com>
2016-01-20 01:19:35 +0900

    ver2

---
+++ Sample.cs
@@ -0,0 +1,33 @@
+﻿/*!
+ * ボタン実行サンプルクラス.
+ * 
+ * @file	Sample.cs
+ * @author	Lotos
+ * @date	2016-01-20 01:16
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Sample : MonoBehaviour
+{
+    #region UI
+    /// <summary>
+    /// サンプルボタン.
+    /// </summary>
+    public NGUIButton sampleBtn;
+    #endregion
+
+    void Start() {
+        // クリックイベントの登録.
+        sampleBtn.Click += SampleBtnClickHandler;
+    }
+
+    /// <summary>
+    /// ボタンの処理.
+    /// </summary>
+    private void SampleBtnClickHandler(object sender, System.EventArgs e) {
+        Debug.Log("Hello World!");
+    }
+
+}
